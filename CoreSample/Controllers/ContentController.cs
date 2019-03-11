6c12bff15d82ed46273e79b8637ef4e510a8f6e6
VinsonWang <vinson_wang@huatek.com>
2019-03-11 17:17:52 +0800

    测试代码

---
+++ CoreSample/Controllers/ContentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CoreSample.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content content;
+        public ContentController(IOptions<Content> option)
+        {
+            content = option.Value;
+        }
+
+        public IActionResult Index()
+        {
+            var contentView = new ContentViewModel();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    var content = new Content
+            //    {
+            //        Id = i,
+            //        title = $"{i}的标题",
+            //        content = $"{i}的内容",
+            //        status = 1,
+            //        add_time = DateTime.Now.AddDays(-i)
+            //    };
+            //    contentView.Contents.Add(content);
+            //}
+            contentView.Contents.Add(content);
+            return View(contentView);
+        }
+    }
+}
---
+++ CoreSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CoreSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                      .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                      //加载自定义json文件
+                      .AddJsonFile("Content.json", optional: false, reloadOnChange: false)
+                      .AddEnvironmentVariables();
+            }).UseStartup<Startup>();
+    }
+}
